6cbcdda704d1a54b405f56bb753ae55284979384
jiangzh <jiangzihao@souche.com>
2020-05-03 14:36:49 +0800

    feat: initialize

---
+++ test/test.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+/**
+ * @author: jiangzh
+ */
+public class test {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("a", "a");
+        System.out.println(map.get("a"));
+    }
+}
